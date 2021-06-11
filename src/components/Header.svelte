3bf2bd7e811735e48e6f8017f46bbac5d128a223
Adam Gniady <adam.gniady@mail.schwarz>
2021-06-11 08:51:43 +0200

    feat: implemented options modal & information modal

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,13 @@
 <script>
-    import Checkbox from './Checkbox.svelte';
+  import {uiState} from "../store";
+  import Checkbox from './Checkbox.svelte';
+  import OptionsModal from "./OptionsModal.svelte";
 
-    export let resetGame;
-    export let startRandomNoteQuiz;
-    export let time;
-    export let withTimeConstraint = true;
-    export let fretboardNavigationQuestions = false;
+  export let resetGame;
+  export let startRandomNoteQuiz;
+  export let time;
+  export let withTimeConstraint = true;
+  export let fretboardNavigationQuestions = false;
 </script>
 
 <style>
@@ -24,31 +26,142 @@
     </div>
     
     <div class="max-h-24 flex justify-center">
-      <button 
+        <button
         class="button"
         on:click={resetGame}>
         Reset
-      </button>
+        </button>
+
+        <button
+            class="button"
+            on:click={startRandomNoteQuiz}
+            >
+            Show Random Note
+        </button>
   
-      <button
-        class="button"
-        on:click={startRandomNoteQuiz}
-        >
-        Show Random Note
-      </button>
-  
-      {#if withTimeConstraint}
+        {#if withTimeConstraint}
         <div
           class="m-4 p-4 border-4 border-green-600 text-green-700 text-center rounded-full w-48"
         >
-          <time class="font-black mr-2" class:overTime={time >= 6}>{time}</time> Seconds
+          <time class="font-black mr-2" class:overTime={time >= $uiState.timing}>{time}</time> Seconds
         </div>
-      {/if} 
+        {/if}
 
-      <div class="flex flex-col justify-center">
-        <Checkbox label="With time constraint?" bind:checked={withTimeConstraint} />
-        <Checkbox label="Fretboardnavigation questions?" bind:checked={fretboardNavigationQuestions}/>
-      </div>
+        <button
+            class="button"
+            on:click={() => uiState.update(uiState => {
+            return {
+                ...uiState,
+                isOptionsModalVisible: true
+            }
+        })}
+        >
+            Options
+        </button>
 
+        <button
+            class="text-green-700"
+            on:click={() => uiState.update(uiState => {
+                return {
+                    ...uiState,
+                    isInfoModalVisible: true
+                }
+            })}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+            </svg>
+        </button>
+
+        {#if $uiState.isOptionsModalVisible}
+            <OptionsModal class="w-96 h-96">
+                <h4 class="text-2xl text-green-700 font-black">
+                    Options
+                </h4>
+                <div class="flex flex-col justify-center">
+                    <Checkbox label="With time constraint?" bind:checked={withTimeConstraint} />
+                    <label class="mt-2 text-left text-lg text-green-700 font-bold">
+                        Timeroptions
+                        <input
+                            class="mt-2 border-2 rounded border-green-700 p-4 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500"
+                            type=number
+                            bind:value={$uiState.timing}
+                        >
+                    </label>
+                </div>
+                <button
+                    class="button"
+                    on:click={() => uiState.update(uiState => {
+                        return {
+                            ...uiState,
+                            isOptionsModalVisible: false
+                        }
+                    })}
+                >
+                    Close
+                </button>
+            </OptionsModal>
+        {/if}
+
+        {#if $uiState.isInfoModalVisible}
+            <OptionsModal class="h-3/5">
+                <h4 class="text-2xl text-green-700 font-black">
+                    Information
+                </h4>
+                <p>
+                    This App is useable with your <strong>keyboard</strong>!
+                </p>
+                <h5 class="text-lg font-bold text-green-700 mt-4">
+                    Keyboard Commands
+                </h5>
+                <table class="border-2 mt-2">
+                    <tr class="p-4">
+                        <th class="p-4">
+                            Command
+                        </th>
+                        <th class="p-4">
+                            Key
+                        </th>
+                    </tr>
+                    <tr>
+                        <td class="p-4 border-2">
+                            Get new random note
+                        </td>
+                        <td class="p-4 border-2">
+                            <code>n</code>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-4 border-2">
+                            Note without accidental
+                        </td>
+                        <td class="p-4 border-2">
+                            <code>c, d, e, f, g, a, b</code>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-4 border-2">
+                            Note with accidental
+                        </td>
+                        <td class="p-4 border-2">
+                            <code>Shift</code>
+                            +
+                            <code>c, d, e, f, g, a, b</code>
+                        </td>
+                    </tr>
+                </table>
+                <button
+                    class="button"
+                    on:click={() => uiState.update(uiState => {
+                        return {
+                            ...uiState,
+                            isInfoModalVisible: false
+                        }
+                    })}
+                >
+                    Close
+                </button>
+            </OptionsModal>
+            {/if}
     </div>
   </header>
---
+++ src/components/OptionsModal.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { blur } from 'svelte/transition';
+  import {onMount} from "svelte";
+
+  onMount(() => {
+    modalWidth = Modal.offsetWidth;
+    modalHeight = Modal.offsetHeight;
+  })
+
+  let Modal;
+  let modalWidth = 0;
+  let modalHeight = 0;
+</script>
+
+<style>
+    .modal {
+        z-index: 1000;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+    }
+</style>
+
+<div
+    class="modal w-auto h-auto bg-white rounded-lg shadow-xl flex flex-col justify-center items-center p-4"
+    transition:blur={{duration: 200}}
+    bind:this={Modal}
+    style="margin-left: -{modalWidth / 2}px; margin-top: -{modalWidth / 2}px;"
+>
+    <slot />
+</div>
